ccacae4eb131ba8aa30992328feeab1c899a9dd9
lamkimphu258 <lamkimphu258@gmail.com>
2021-08-20 09:19:52 +0700

    TODO-8 Add create todo

---
+++ src/Controller/TodoController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use TodoType;
+
+class TodoController extends AbstractController
+{
+    public function __construct(
+        protected TodoRepository $toDoRepository
+    ) {
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    #[Route(
+        '/todo',
+        name: 'todo-create',
+        methods: [Request::METHOD_GET, Request::METHOD_POST]
+    )]
+    public function create(Request $request): Response
+    {
+        $form = $this->createForm(TodoType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $todo = $form->getData();
+            $this->toDoRepository->save($todo);
+            $this->addFlash('success', 'Todo Created');
+
+            return $this->redirectToRoute('todo-create');
+        }
+
+        return $this->render('todos/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Entity/Todo.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Id\UuidGenerator;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UuidGenerator::class)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
