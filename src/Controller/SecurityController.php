c2b105d9cecb1af5c322364e68998deb81471256
lamkimphu258 <lamkimphu258@gmail.com>
2021-08-26 20:41:44 +0700

    Add auto login user after register and error message when user with email is already existed

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,13 +5,15 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\Type\UserType;
 use App\Repository\UserRepository;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Authentication\AuthenticatorManagerInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -39,19 +41,36 @@ class SecurityController extends AbstractController
      * @return Response
      */
     #[Route("/register", name: "app_register")]
-    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        LoginFormAuthenticator $authenticator,
+        UserAuthenticatorInterface $userAuthenticator
+    ): Response
     {
         $form = $this->createForm(UserType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var User $userData */
             $userData = $form->getData();
+            $isExistedUser = $this->userRepository->findByEmail($userData->getEmail());
+            if (!is_null($isExistedUser)){
+                $this->addFlash('error', 'User with this email is already existed');
+                return $this->redirectToRoute('app_register');
+            }
+
             $user = new User();
             $user->setEmail($userData->getEmail());
             $user->setPassword(
                 $passwordHasher->hashPassword($user, $userData->getPassword())
             );
             $this->userRepository->save($user);
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
         }
 
         return $this->render('security/register.html.twig', [
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
@@ -16,7 +17,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator implements AuthenticatorInterface
 {
     use TargetPathTrait;
 
